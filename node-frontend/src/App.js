4d0ed3de7aaf9fcd7628f8e33260d93351363527
Igor dos Santos Amado <igorsamado@gmail.com>
2021-10-14 19:34:15 -0300

    Componente principal App do projeto frontend

---
+++ node-frontend/src/App.js
@@ -0,0 +1,13 @@
+// import logo from "./logo.svg";
+import "./App.css";
+import Alunos from "./Alunos.js";
+
+function App() {
+  return (
+    <div className="App">
+      <Alunos />
+    </div>
+  );
+}
+
+export default App;
